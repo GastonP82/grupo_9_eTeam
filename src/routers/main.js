c8f4856c995ae419cfd4f24b0db30c11754aa65a
paola vera <paola.1208vera@gmail.com>
2022-06-01 20:32:10 -0500

    cuarto commit

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const mainController = require("../../controllers/mainController");
+const mainController = require("../controllers/mainController");
 
 const router = express.Router();
 
